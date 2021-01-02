d5c3aff3160b9c1d5ae204efa24770201eb731a5
SkyWalker2506 <42008533+SkyWalker2506@users.noreply.github.com>
2021-01-02 03:14:34 +0300

    Initial

---
+++ Runtime/Scripts/SequentialMoveController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace SequentialMovementSystem
+{
+
+    public class SequentialMoveController : MonoBehaviour
+    {
+        Transform moveObject;
+        int currentIndex;
+        SequentialMoveData sequentialMoveData;
+        float tick = .1f;
+        Coroutine moveCoroutine;
+        Coroutine moveNextCoroutine;
+        public Action OnMoveEnded;
+
+        public void SetMoveObject(Transform moveObject)
+        {
+            this.moveObject = moveObject;
+        }
+
+        public void SetMoveData(SequentialMoveData sequentialMoveData)
+        {
+            this.sequentialMoveData = sequentialMoveData;
+        }
+
+        public void StartMovingObject()
+        {
+            if(moveObject==null)
+                throw new ArgumentNullException("No object to move. Set object to move.");
+            currentIndex = 0;
+            if(sequentialMoveData.GetPositions().Count==0)
+                throw new ArgumentNullException("No positions set to move.");
+            moveObject.position = sequentialMoveData.GetPositions()[0];
+            StartCoroutine(IEStartMoveToTarget());
+        }
+
+        public void StopMovingObject()
+        {
+            if (moveObject == null)
+                throw new ArgumentNullException("No object to stop moving.");
+            if(moveCoroutine!=null)
+                StopCoroutine(moveCoroutine);
+            if (moveNextCoroutine != null)
+                StopCoroutine(moveNextCoroutine);
+        }
+
+        public void ResumeMovingObject()
+        {
+            if (moveObject == null)
+                throw new ArgumentNullException("No object to move. Set object to move.");
+            moveCoroutine = StartCoroutine(IEStartMoveToTarget());
+        }
+
+        IEnumerator IEStartMoveToTarget()
+        {
+            for (int i = currentIndex; i < sequentialMoveData.GetPositions().Count-1; i++)
+            {
+                moveNextCoroutine = StartCoroutine(MoveToNextTarget());
+                yield return moveNextCoroutine;
+            }
+            OnMoveEnded?.Invoke();
+
+        }
+
+        IEnumerator MoveToNextTarget()
+        {
+            var currentPosition = moveObject.position;
+            var currentRotation = moveObject.rotation;
+            var targetPosition = sequentialMoveData.GetPositions()[currentIndex+1];
+            var moveDirection = (targetPosition - currentPosition).normalized;
+            var targetRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+            var distance = Vector3.Distance(currentPosition, targetPosition);
+            var step = tick/distance;
+            float time = 0;
+            while (time<1)
+            {
+                time += step;
+                moveObject.position = Vector3.Lerp(currentPosition, targetPosition, time);
+                moveObject.rotation = Quaternion.Lerp(currentRotation, targetRotation,time);
+                yield return null;
+            }
+            currentIndex++;
+        }
+
+    }
+
+}
---
+++ Runtime/Scripts/SequentialMoveData.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SequentialMovementSystem
+{
+    public class SequentialMoveData : ScriptableObject
+    {
+        [SerializeField]List<Vector3> movePositions;
+
+
+        public SequentialMoveData()
+        {
+            movePositions = new List<Vector3>();
+        }
+        public SequentialMoveData(List<Vector3> movePositions=null)
+        {
+            if (movePositions != null)
+                this.movePositions = movePositions;
+            else
+                this.movePositions = new List<Vector3>();
+        }
+
+        public SequentialMoveData(Vector3[] movePositions = null)
+        {
+            this.movePositions = new List<Vector3>();
+            if (movePositions != null)
+                this.movePositions.AddRange(movePositions);
+        }
+
+        public void ClearData()
+        {
+            movePositions = new List<Vector3>();
+        }
+
+        public void AddPosition(Vector3 position)
+        {
+            movePositions.Add(position);
+        }
+
+        public void AddPositions(List<Vector3> positions)
+        {
+            movePositions.AddRange(positions);
+        }
+
+        public List<Vector3> GetPositions()
+        {
+            return movePositions;
+        }
+
+    }
+
+}
